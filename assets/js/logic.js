6c308d9fe70b853e8c9f00d43d0e015553c45194
Joshua Hawks <jdhawks@gmail.com>
2022-08-09 22:42:53 -0500

    pseudo code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -46,17 +46,12 @@ const feedbackEl = document.getElementById('feedback');
 
 function startQuiz() {
 	// hide start screen
-	const startScreenEl = document.getElementById('start-screen');
-	startScreenEl.setAttribute('class', 'hide');
 
 	// un-hide questions section
-	questionsEl.removeAttribute('class');
 
 	// start timer
-	timerId = setInterval(clockTick, 1000);
 
 	// show starting time
-	timerEl.textContent = time;
 
 	// get first question
 	getQuestion();
@@ -64,103 +59,38 @@ function startQuiz() {
 
 function getQuestion() {
 	// get current question object from array
-	let currentQuestion = questions[currentQuestionIndex];
-
 	// update title with current question
-	const titleEl = document.getElementById('question-title');
-	titleEl.textContent = currentQuestion.title;
-
 	// clear out any old question choices
-	choicesEl.innerHTML = '';
-
 	// loop over choices
-	for (let i = 0; i < currentQuestion.choices.length; i++) {
-		// create new button for each choice
-		let choice = currentQuestion.choices[i];
-		let choiceNode = document.createElement('button');
-		choiceNode.setAttribute('class', 'choice');
-		choiceNode.setAttribute('value', choice);
-
-		choiceNode.textContent = choice;
-
-		// display on the page
-		choicesEl.appendChild(choiceNode);
-	}
+	// create new button for each choice
+	// display on the page
 }
 
 function questionClick(event) {
-	let buttonEl = event.target;
-
 	// if the clicked element is not a choice button, do nothing.
-	if (!buttonEl.matches('.choice')) {
-		return;
-	}
-
 	// check if user guessed wrong
-	if (buttonEl.value !== questions[currentQuestionIndex].answer) {
-		// penalize time
-		time -= 15;
-
-		// show feedback
-		if (time < 0) {
-			// no such thing as negative time....
-			time = 0;
-		}
-
-		// display new time on page
-		timerEl.textContent = time;
-
-		feedbackEl.textContent = 'Wrong!';
-	} else {
-		feedbackEl.textContent = 'Correct!';
-	}
-
+	// penalize time
+	// show feedback
+	// no such thing as negative time....
+	// display new time on page
+	// give user feedback for correct or incorrect guess
 	// flash right/wrong feedback on page for half a second
-	feedbackEl.setAttribute('class', 'feedback');
-	setTimeout(function () {
-		feedbackEl.setAttribute('class', 'feedback hide');
-	}, 500);
-
 	// move to next question
-	currentQuestionIndex++;
-
 	// check if we've run out of questions
-	if (time <= 0 || currentQuestionIndex === questions.length) {
-		quizEnd();
-	} else {
-		getQuestion();
-	}
 }
 
 function quizEnd() {
 	// stop timer
-	clearInterval(timerId);
-
 	// show end screen
-	const endScreenEl = document.getElementById('end-screen');
-	endScreenEl.removeAttribute('class');
-
 	// show final score
-	const finalScoreEl = document.getElementById('final-score');
-	finalScoreEl.textContent = time;
-
 	// hide questions section
-	questionsEl.setAttribute('class', 'hide');
 }
 
 function clockTick() {
 	// update time
-	time--;
-	timerEl.textContent = time;
-
 	// check if user ran out of time
-	if (time <= 0) {
-		quizEnd();
-	}
 }
 
 // user clicks button to start quiz
-startBtn.onclick = startQuiz;
 
 // user clicks on element containing choices
-choicesEl.onclick = questionClick;
